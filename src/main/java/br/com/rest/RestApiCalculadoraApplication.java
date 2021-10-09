c9c1534b534db11c53c3be05881b62bf6f4a053e
Luciano de Paula <luciano.desenvolvedor@gmail.com>
2021-10-09 08:38:03 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/rest/RestApiCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCalculadoraApplication.class, args);
+	}
+
+}
